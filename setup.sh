70ff7b4de03c4b38c5ae3cef3a400891abfeec06
Roberto Pio Iannello <s5201538@studenti.unige.it>
2025-11-15 18:08:22 +0100

    Added git setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Update fedora and install frequently used packages"
 sudo dnf update -y                          					# update everything
 sudo dnf install distrobox podman-docker podman-compose -y      # install distrobox and podman goodies
 sudo dnf install bat -y											# install bat (cool version of cat)
-sudo dnf install nextcloud-client -y
+sudo dnf install nextcloud-client -y							# for nextcloud syncronization
 
 echo "Installing flatpaks"
 xargs flatpak install -y --noninteractive flathub < flatpaks.txt
@@ -13,6 +13,17 @@ xargs flatpak install -y --noninteractive flathub < flatpaks.txt
 # https://github.com/containers/buildah/issues/2959
 echo "Change BUILDAH_FORMAT=docker to build Dockerfile with podman with no issues"
 sudo echo 'BUILDAH_FORMAT=docker' | sudo tee -a /etc/environment # build Dockerfiles the same way as docker
+
+echo "Set up Git with GitHub CLI"
+sudo dnf install gh	-y											# better github integration
+gh auth login
+gh auth setup-git
+
+
+read -p "Insert username for git" username
+git config --global user.name $username
+read -p "Insert username for git" mail
+git config --global user.email $mail
 
 # additional configuration for desktop 
 read -p "Apply additional config for your desktop? (y/n)" confirm
